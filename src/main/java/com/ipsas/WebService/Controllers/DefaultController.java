ffde267461bb4877c25305de1003bc1a0f38f673
skanderhk <skander.hkacem@gmail.com>
2021-06-23 13:05:06 +0200

    Add swagge infos

--- src/main/java/com/ipsas/WebService/Controllers/DefaultController.java
+++ src/main/java/com/ipsas/WebService/Controllers/DefaultController.java
@@ -6,25 +6,20 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirements;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
-@OpenAPIDefinition(info = @Info(title = "Florist API",description = "Client Controller using OpenAPI",version = "2.0",
+@OpenAPIDefinition(info = @Info(title = "Florist API",description = "Client Controller using OpenAPI",version = "${build.version}",
         contact = @Contact(
                 name = "SkanderHk",
                 email = "Skander.hkacem@gmail.com"))
                     )
 public class DefaultController {
-
     private final UserService userService;
 
     public DefaultController(UserService userService) {
